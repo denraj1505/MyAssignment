769a5b1e02f72f9b2f00cbee2c06a69267eab59e
denraj1505 <114810904+denraj1505@users.noreply.github.com>
2022-10-01 17:40:41 +0530

    Add files via upload
    Day1

---
+++ day1/Factorial.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int fact=1;  
+		   
+		  for(int i=1;i<=5;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Output: 5*4*3*2*1 = "+fact);    
+		 }  
+		
+
+	}
+
+
